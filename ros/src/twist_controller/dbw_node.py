1b044ea618314a797b02b31d979f5bd8d6bec39d
Mwesi <mwesi@yahoo.com>
2022-11-27 11:57:48 -0500

    Update dbw_node.py

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -58,7 +58,8 @@ class DBWNode(object):
         self.current_velocity = None
 
         # TODO: Create `Controller` object
-        self.controller = Controller(cp=cp)
+        self.controller = Controller(vehicle_mass, fuel_capacity, brake_deadband, decel_limit, **accel_limit, **wheel_radius, **wheel_base, **steer_ratio, **max_lat_accel, **max_steer_angle)
+                
 
         # TODO: Subscribe to all the topics you need to
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_cb, queue_size=5)
