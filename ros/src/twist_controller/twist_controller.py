ca6e56202fc44c44393cfb663da9de3e4ecae333
Mwesi <mwesi@yahoo.com>
2022-12-03 07:25:16 -0500

    Update twist_controller.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,6 +4,13 @@ import rospy
 
 from yaw_controller import YawController
 from pid import PID
+from lowpass import LowPassFilter
+
+
+
+
+
+
 
 
 GAS_DENSITY = 2.858
@@ -14,8 +21,7 @@ class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
         self.yaw_controller = YawController (wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
-        
-        
+      
         kp = 0.3
         ki = 0.1
         kd = 0
@@ -26,12 +32,12 @@ class Controller(object):
         tau = 0.5 # 1/(2pi*tau) = cutoff frequency
         ts = .02 # Sample time
         self.vel_lpf = LowPassFilter(tau, ts)
-        self.vehicle_mass = LowPassFilter(tau, ts)fuel_capacity
+        self.vehicle_mass = LowPassFilter(tau, ts)
         self.fuel_capacity = fuel_capacity
-        self.brake deadband = brake_deadband
-        self.decel limit = decel_limit
-        self.accel limit = accel_limit
-        self.wheel radius = wheel_radius
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
         
         self.last_time = rospy.get_time()
 
@@ -39,13 +45,13 @@ class Controller(object):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
         
-        if not dbw enabled:
-            self.throttle controller.reset() 
+        if not dbw_enabled:
+            self.throttle_controller.reset() 
             return 0., 0., 0.
         
         current_vel = self.vel_lpf.filt(current_vel)
         
-        steering = self.yaw controller.get_steering (linear_vel, angular_vel, current_vel)
+        steering = self.yaw_controller.get_steering (linear_vel, angular_vel, current_vel)
         
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
@@ -54,7 +60,7 @@ class Controller(object):
         sample_time = current_time - self.last_time
         self.last_time = current_time
         
-        throttle = self.throttle controller.step(vel_error, sample_time)
+        throttle = self.throttle_controller.step(vel_error, sample_time)
         brake = 0
         
         if linear_vel == 0. and current_vel < 0.1:
